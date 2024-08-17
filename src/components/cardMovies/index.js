f1d02e5bc40fe997cebbd510681aa63bb39597f1
FernandoRamirez457 <ramirezgutierrezfernando457@gmail.com>
2024-08-16 22:25:04 -0300

    Atualizando e Arrumando Página Details

--- src/components/cardMovies/index.js
+++ src/components/cardMovies/index.js
@@ -1,28 +1,32 @@
-import React from 'react'
-import { View, Image, Text, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native'
+import React from "react";
+import { View, Image, Text, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-import styles from './style';
-
+import styles from "./style";
 
 export default function CardMovies({ titulo, nota, imagem }) {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    return (
-        <>
-            <TouchableOpacity style={styles.containerJogos} onPress={() => navigation.navigate('Details', { titulo: titulo, nota: nota, imagem: imagem })}>
+  return (
+    <>
+      <TouchableOpacity
+        style={styles.containerJogos}
+        onPress={() =>
+          navigation.navigate("Details", {
+            titulo: titulo,
+            nota: nota,
+            imagem: imagem,
+          })
+        }
+      >
+        <Image
+          style={styles.images}
+          source={{ uri: `https://image.tmdb.org/t/p/original/${imagem}` }}
+        />
+        <Text style={styles.titulo}>{titulo} </Text>
 
-                <Image style={styles.images} source={{uri:(`https://image.tmdb.org/t/p/original/${imagem}`)}} />
-                <Text style={styles.titulo}>{titulo} </Text>
-
-                <Text style={styles.textNota}> {nota} </Text>
-
-
-            </TouchableOpacity>
-        </>
-
-
-    );
-
-
+        <Text style={styles.textNota}> {nota} </Text>
+      </TouchableOpacity>
+    </>
+  );
 }
--- src/screens/Details/index.js
+++ src/screens/Details/index.js
@@ -1,45 +1,41 @@
-import { useNavigation, useRoute } from '@react-navigation/native'
-import React from 'react'
-import {View, Text,Image, TouchableOpacity} from 'react-native'
-import styles from './styles';
-import { AntDesign } from '@expo/vector-icons';
-import Stars from 'react-native-stars';
-import { Ionicons } from '@expo/vector-icons';
-
+import { useNavigation, useRoute } from "@react-navigation/native";
+import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import styles from "./styles";
+import { AntDesign } from "@expo/vector-icons";
+import Stars from "react-native-stars";
+import { Ionicons } from "@expo/vector-icons";
 
 // instalar a biblioteca das starts : npm install react-native-stars --save
 
-export default function Details(){
- const route = useRoute();
- const navigation = useNavigation();
-    return(
-            <View style ={styles.viewContainer}>
-
-      
-          
-
-         
-          <Image resizeMode='stretch' style={styles.imageBanner} source ={require(`../../Img/${route.params.imagem}`)} />
-          <TouchableOpacity style ={styles.backButton} onPress={navigation.goBack}> 
-          <AntDesign  name="leftcircle" size={35} color="white" /> 
-          </TouchableOpacity>
-          <View style={{marginTop:350}}>
-          <Stars
-            disabled = {true}
-            default = {route.params?.nota}
-            count = {10}
-            half = {true}
-            starSize ={20}
-            fullStar = {<Ionicons name='md-star' size={24} color='#E7A74e'/>}
-            emptyStar = {<Ionicons name='md-star-outline' size={24} color='#E7A74e'/>}
-            halfStar = {<Ionicons name='md-star-half' size={24} color='#E7A74e'/>}>
-
-            </Stars>
-            </View>
-          <Text style ={styles.textBanner}> {route.params?.titulo}</Text>
-            </View> 
-    
-        )
-
-
+export default function Details() {
+  const route = useRoute();
+  const navigation = useNavigation();
+  return (
+    <View style={styles.viewContainer}>
+      <Image
+        resizeMode="stretch"
+        style={styles.imageBanner}
+        source={{ uri: `https://image.tmdb.org/t/p/original/${route.params?.imagem}` }}
+      />
+      <TouchableOpacity style={styles.backButton} onPress={navigation.goBack}>
+        <AntDesign name="leftcircle" size={35} color="white" />
+      </TouchableOpacity>
+      <View style={{ marginTop: 350 }}>
+        <Stars
+          disabled={true}
+          default={route.params?.nota}
+          count={10}
+          half={true}
+          starSize={20}
+          fullStar={<Ionicons name="md-star" size={24} color="#E7A74e" />}
+          emptyStar={
+            <Ionicons name="md-star-outline" size={24} color="#E7A74e" />
+          }
+          halfStar={<Ionicons name="md-star-half" size={24} color="#E7A74e" />}
+        ></Stars>
+      </View>
+      <Text style={styles.textBanner}> {route.params?.titulo}</Text>
+    </View>
+  );
 }
